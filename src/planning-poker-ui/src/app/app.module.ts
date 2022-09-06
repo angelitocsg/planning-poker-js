9ac0291e5af7d9eb7faa9ab8d58c552afee4aa64
Angelito Casagrande <angelito@angelito.com.br>
2022-09-05 22:59:18 -0300

    Bootstrap blazor version components

--- src/planning-poker-ui/src/app/app.module.ts
+++ src/planning-poker-ui/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { CardTextComponent } from './components/card-text/card-text.component';
+import { GameTableComponent } from './components/game-table/game-table.component';
+import { PlayerModalComponent } from './components/player-modal/player-modal.component';
+import { PlayerTableComponent } from './components/player-table/player-table.component';
+import { SessionTimerComponent } from './components/session-timer/session-timer.component';
+import { SummaryTableComponent } from './components/summary-table/summary-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardTextComponent,
+    GameTableComponent,
+    PlayerModalComponent,
+    PlayerTableComponent,
+    SessionTimerComponent,
+    SummaryTableComponent,
   ],
   imports: [
     BrowserModule,
