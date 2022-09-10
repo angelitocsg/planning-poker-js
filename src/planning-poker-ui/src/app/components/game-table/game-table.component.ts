9bb045ed803c49b9652108adb7a0ab86cbb0a5d3
Angelito Casagrande <angelito@angelito.com.br>
2022-09-09 23:15:14 -0300

    Implement game table and other fixes

--- src/planning-poker-ui/src/app/components/game-table/game-table.component.ts
+++ src/planning-poker-ui/src/app/components/game-table/game-table.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { SessionTimerComponent } from '../session-timer/session-timer.component';
 
 @Component({
   selector: 'app-game-table',
   templateUrl: './game-table.component.html',
-  styleUrls: ['./game-table.component.scss']
+  styleUrls: ['./game-table.component.scss'],
 })
 export class GameTableComponent implements OnInit {
+  @Input() active: boolean = false;
+  @Input() descriptionFromHub?: string ;
 
-  constructor() { }
+  @Output() onDescriptionChange = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  @ViewChild(SessionTimerComponent) sessionTimerViewChild?: SessionTimerComponent;
+
+  formModel = this.formBuilder.group({
+    description: this.descriptionFromHub,
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  getGameTableActiveStyle() {
+    return this.active ? 'highlight' : '';
   }
 
+  getGameTableStyle() {
+    return `game-table ${this.getGameTableActiveStyle()}`;
+  }
+
+  handleFormSubmit() {
+    console.log('formModel.description', this.formModel.get('description')?.value);
+
+    if (this.descriptionFromHub)
+      this.onDescriptionChange.emit(this.descriptionFromHub);
+  }
 }
