75385e3f0a3187b604631471c4dc94ffcca4008f
Angelito Casagrande <angelito@angelito.com.br>
2022-09-19 23:42:42 -0300

    Update create page component

--- src/planning-poker-ui/src/app/pages/create/create.component.ts
+++ src/planning-poker-ui/src/app/pages/create/create.component.ts
@@ -1,33 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject, map, Observable, Subscription } from 'rxjs';
+import { GameSessionViewModel } from '../../models/game-session-view-model';
+
 import { SessionService } from '../../services/session.service';
+import { updateLocalPlayerNameAction } from '../../store/game.action';
+import { IStateContainer } from '../../store/state.container';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
 })
-export class CreateComponent implements OnInit {
-  ownerName: string = '';
+export class CreateComponent implements OnInit, OnDestroy {
   loading: boolean = false;
   get labelOk(): string {
     return this.loading ? 'criando...' : 'criar sessão';
   }
 
-  constructor(private _sessionService: SessionService) {}
+  private stateContainer!: Observable<IStateContainer>;
+  private stateSubscription!: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    private sessionService: SessionService,
+    private store: Store<{ stateContainer: IStateContainer }>,
+    private router: Router
+  ) {}
 
-  handleEnter(ownerName: string) {
-    this.loading = true;
-
-    if (!ownerName) return;
-
-     this._sessionService.createSession(ownerName);
+  ngOnInit(): void {
+    this.stateContainer = this.store.select('stateContainer');
+    this.stateSubscription = this.stateContainer.subscribe((state) => {
+      this.enterToSession(state.gameSession);
+    });
   }
 
-  enterToSession() {
+  ngOnDestroy(): void {
+    this.stateSubscription?.unsubscribe();
+  }
+
+  handleEnter(ownerName: string) {
+    this.store.dispatch(updateLocalPlayerNameAction({ localPlayerName: ownerName }));
+    if (!ownerName) return;
+    this.loading = true;
+    this.sessionService.createSession(ownerName);
+  }
+
+  enterToSession(gameSession: GameSessionViewModel) {
     this.loading = false;
-    // console.log("Sessão criada: {0}", _stateContainer?.GameSession?.Id);
-    // _navigationManager.NavigateTo($"/session/{_stateContainer?.GameSession?.Id}");
+    console.log('Sessão criada:', gameSession.id);
+    this.router.navigate([`session/${gameSession.id}`]);
   }
 }
